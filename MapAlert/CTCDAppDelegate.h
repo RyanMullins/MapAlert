d263c683d39c9eb4e25c608338a5fa3f46799fdb
Ryan Mullins <rsm5068@gmail.com>
2013-01-21 10:48:01 -0500

    Initial Commit
    Created a new Xcode project for the MapAlert project. This project is
    an empty application that will be added to as features, views, and
    content are fleshed out.

---
+++ MapAlert/CTCDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTCDAppDelegate.h
+//  MapAlert
+//
+//  Created by Ryan Mullins on 1/21/13.
+//  Copyright (c) 2013 com.cartocadabra.mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTCDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapAlert/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapAlert
+//
+//  Created by Ryan Mullins on 1/21/13.
+//  Copyright (c) 2013 com.cartocadabra.mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CTCDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CTCDAppDelegate class]));
+    }
+}
